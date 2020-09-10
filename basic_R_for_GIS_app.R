1b4f66bc49a03643bd44398d69614d7fdeb2512d
Daniel Romero-Alvarez <daromero88@gmail.com>
2020-09-09 21:22:10 -0500

    definitive version

--- basic_R_for_GIS_app.R
+++ basic_R_for_GIS_app.R
@@ -32,7 +32,7 @@
  #' https://desktop.github.com/
  #' 
  #' Code repository: 
- #' 
+ #' https://github.com/daromero-88/Basic-R-for-GIS-applications
  #' 
  #' Endless other resources... 
  #' 
@@ -47,18 +47,18 @@
 
 a = 1
 
-a =  1, 2, 3 #error, complex 'c()' object not defined
+a =  1, 2, 3 #error, complex 'c()' object not defined, never forget to actually READ THE ERROR screen
 
 a = c(1, 2, 3)
 
-class(a) 
+class(a) #this function tells you what is happending with the code 
 #numeric vector
 
 b = tttt #error, no character defined
 
 b = 'Hello World' #characters go with quotes
 
-b = 'a', 'b', 'c' #error no complex number
+b = 'a', 'b', 'c' #error no complex object defined
 
 b = c('a', 'b', 'c')
 
@@ -77,65 +77,23 @@ class (list1)
 #Subsetting: 
 cc = c(2000:4000)
 
-
-cc[1]
-cc[2]
+cc[1] #subset to obtain the first element 
+cc[2] #subset to obtain the second element 
 cc[10:20] #subsetting the numeric vector cc to obtain the elementes from 10 to 20
 
 
-cc[which (cc== 2090)]
-
-#1.2. Creatig data: Dataframes and matrices-----------------------------
-
-c1 = rnorm (100, 5, 2)
-hist (c1)
-plot (c1)
-plot (sort(c1))
-
-c2 = sample (1:1000,100, replace = T)
-hist (c2)
-plot (c2)
-
-c3 = runif (100, 0.1, 5)
-hist (c3)
-
-df1 = data.frame (cbind (c1, c2, c3))
-class (df1)
-
-colnames (df1) = c('First', 'Second', 'Third')
-head (df1)
-tail (df1)
-
-#Dataframe focus on columns as variables and rows as observations
-
-m1 = matrix (ncol = 10, nrow = 10, sample (1:20,100, replace = T))
-
-#structure of the information, it can be a square, a line, a rectangle, etc...columns and rows are not variables and observations
+cc[which (cc== 2090)] #subset by using logical paramaters: '==' is the way to tell R: EQUALS, because the normal '=' is used to defined objects
 
 
-#Subsetting: 
-
-df1$Second
-
-df1[,2]
-
-df1[1,1]
-
-df1[1:5,3]
-
-df1[3, 1:3]
-
-df1[3,]
-
-
-
-
-# 1.3. In build functions, operations -----------------------------------
+# 1.2. In build functions, operations -----------------------------------
 
 #Numeric vectors: 
 
 e = 1001:2000
 f = 1:1000
+
+length(e)
+length(f)
 
 class(e)
 
@@ -154,56 +112,131 @@ h+i
 
 summary (e)
 
-setwd ('/Users/daniel/Documents/GitHub/Basic-R-for-GIS-applications')
-getwd
+#1.3. Creatig data: Dataframes and matrices-----------------------------
+#Check section 2 before analyzing section 1.2
+c1 = rnorm (100, 5, 2) #creates a numeric vector with a random distribution with mean = 5 and standard deviation = 2
+hist (c1) #create a histogram 
+plot (c1) #create a plot of the points (notice the difference between histogram and plot)
+plot (sort(c1)) #same plot with sorted data via the function sort
+
+c2 = sample (1:1000,100, replace = T) #creates a numeric vector using a sample 
+hist (c2) #histogram 
+plot (c2) #plot
+
+c3 = runif (100, 0.1, 5) #function to create a uniform distributioon 
+hist (c3) #histogram 
+
+df1 = data.frame (cbind (c1, c2, c3)) #create a dataframe with the objects previously created
+class (df1) #checking the object
+
+colnames (df1) = c('First', 'Second', 'Third') #adding names to the columns in the dataframe
+head (df1) #checking the first 6 objects in the dataframe
+tail (df1) #checking the last 6 objects in the dataframe
+
+#' Dataframe focus on columns as variables and rows as observations
+#' 
+#' 
+#' On the contrary, matrices focus on the distriibution of the data itself, 
+#' columns and rows define information by cell and do not respect the order column = 
+#' variable and row = observation as in dataframes. 
+
+m1 = matrix (ncol = 10, nrow = 10, sample (1:20,100, replace = T)) #function matrix which is respective arguments allows the creation of different columns and rows 
+
+
+#Subsetting matrices and dataframes: 
+
+df1$Second #the operator '$' allows the selection of an entire column 
+
+df1[,2] #square brackets allow the subsetting, in this case of the second column 
+
+df1[1,1] #select the element defined by the indexes of the column and row, in this case first row and first column 
+
+df1[1:5,3] #select the first 5 rows from the third column 
+
+df1[3, 1:3] #select the third element from columns 1 to 3 
+
+df1[3,] #select the entire third row 
+
 
 #2. Anatomy of a function -----------------------------------
 
 #function sample
-j = sample (1:10, 20, replace = T) #check help 
+j = sample (1:100, 300, replace = T) #check help 
 
 #function runif
 k = runif (10, 1.52, 2.80) #check help 
 
 #function round
-l = round (19/456, 4) #check help 
+l = round (19/456, 2) #check help 
 
 #function 
-length(e)
+length(e) #check help 
 
 #function plot 
-plot (j)
+plot (j) #check help 
 
-plot (j, main = 'Example', pch = 15, cex = 0.9, col = 'red')
+plot (j, main = 'Example', pch = 16, 
+      cex = 1.3, col = 'red', 
+      ylab = 'EXAMPLE Y AXIS')
+
+#' each function comes with a particular set of arguments that change
+#' depending on the developer of the function... you can see this by comparing 
+#' sample and runif... both basically create numeric vectors with random numbers 
+#' but each argument should be called inside the function... 
 
 #about color: 
-col1 = c('yellow', 'red')
-pale1 = colorRampPalette(col1)
+col1 = c('yellow', 'red') #vector of two colors as defined in: http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+#there are A LOT OF COLORS AVAILABLE! 
 
-plot (j, main = 'Example', pch = 15, cex = 0.9, col = pale1(5))
+pale1 = colorRampPalette(col1) #function colorRampPalette will create a ramp between the colors presented 
+
+plot (j, main = 'Example', pch = 15, cex = 0.9, col = pale1(5)) #check the plot with your own palette
+
+
+#2.1. Most important function ever: DEFINE WORKING DIRECTORY------------------
+
+#' This function allows the user to select a folder in their computer to add any particular 
+#' output outside R memory. Also from here, elements can be read it to R.  
+
+setwd ('/Users/daniel/Documents/GitHub/Basic-R-for-GIS-applications') #add the corresponding path in your computer 
+
+getwd #this one tells you where is the current directory 
+
 
 #####SUMMARY: 1 and 2#####
 
-#' R and R studio have a large community of support on internet and we must take the advantage
-#' It is a language and as so, you need to learn its syntax and vocubulary 
-#' There are multiple objects in R, usually the objects determine what is happening 
-#' Functions are embebed codes that allows the user to automatize operations 
-#' Dataframes and matrices are complex objects that allow the manipulation of data 
+#' R and R studio have a large community of support on internet and we must take the advantage of 
+#' this strength, make sure to google all your problems. 
+#' It is a language and as so, you need to learn its syntax and vocubulary, for example, 
+#' respect lower/upper cases.
+#' There are multiple objects in R, usually the objects determine what is happening, 
+#' in doubt, use class(THE OBJECT).
+#' Functions are embebed codes that allows the user to automatize operations, 
+#' don't forget to look functions in the HELP section.
+#' Dataframes and matrices are complex objects that allows the storage of 
+#' higher level of complexity than vectors. 
 #' 
 
 #3. GIS objects I: Polygons -----------------------------------
 
 #3.1. Packages and libraries---------------------------------------------
 
-install.packages('raster')
-install.packages('sp')
-install.packages('maptools')
+install.packages('raster') #manipulation of raster files 
+install.packages('sp') #manipulation of shape files (POLYGONS)
+install.packages('rgeos') #manipulation of GIS data 
+install.packages('rgdal') #manipulation of GIS data 
+install.packages('mapdata') #manipulation of GIS data 
+install.packages('maptools') #in-build maps and geographical information. 
 
-#pcks = c('raster', 'sp', 'rgeos', 'rgdal', 'maptools', 'mapdata')
+#' Another way to install everything can be to define a vector of characters
+#' with all the packages that we need for any particular excercise. 
+
+#pcks = c('raster', 'sp', 'rgeos', 'rgdal', 'maptools', 'mapdata') 
 #install.packages(pcks)
 
 #...etc
 
+#The following lines will activate the packages installed 
 library(raster)
 library(sp)
 library(rgeos)
@@ -214,45 +247,75 @@ library(mapdata)
 
 #3.2. Spatial Polygons with  'World Simple'-------------------------
 
-data("wrld_simpl", package = "maptools")
-plot (wrld_simpl)
+data("wrld_simpl", package = "maptools") #this will call the simplified map from maptools package
+plot (wrld_simpl) #checking the whole world 
+
+class (wrld_simpl) #SpatialPolygonsDataFrame object! 
+
+#by calling the object you can see that it has multiple elements: 
+wrld_simpl
+
+#among them: 
+#' features: number of rows in the polygon dataframe
+#' extent: square defining the space of the polygon 
+#' crs: projection (planar, UTM, etc)
+#' variables: number of columns in the polygon dataframe
+
+crs(wrld_simpl) #checking the projection of the object using function crs
+WGS84 = crs(wrld_simpl) # defining a character vector with the polygon projection
 
 
-class (wrld_simpl) #SpatialPolygonsDataFrame
+
+plot (wrld_simpl) #checking the whole world 
+zoom(wrld_simpl, ext=drawExtent(), new=TRUE, useRaster=TRUE) #zooming on the world polygon 
+
+plot (wrld_simpl) #checking the whole world 
+e = drawExtent() #manually selection a portion of the map using a square 
+
+cp1 = as (extent(e), 'SpatialPolygons') #transforming our square in an actual Spatial Polygon 
+
+mp1 = crop (wrld_simpl, cp1) #cropping the map with the just created Spatial Polygon 
+plot (mp1) #plotting the new portion of the map 
+
+#we can write this new shape file in our computer: 
+
+writeOGR(mp1, layer= 'mp1', dsn = 'shapes1', driver = 'ESRI Shapefile') #check help 
+
+#' notice the arguments of this function
+#' layer: name of the output shape file 
+#' dsn: name of a folder that is going to be created where this shapefile will be saved
+#' drive: define the typical shapefile class 
+
+#' now you can check the folder 'shapes1' that is inside the folder of your working directory, 
+#' and there you will find the shapefile you just created...notice that it has different files
+#' a normal shapefile usually is defined by the shape itself (.shp) together with the projection 
+#' (.prj), dataframe (.dbf), and index of the shape (.shx)
+#' 
+
+#we can read the shapefile that we just created as follows: 
+
+ex1 = readOGR ('./shapes1/mp1.shp') #defining an object using the path of the .shp file extension 
+plot (ex1, col = 'grey', main = 'Reading maps') #we can plot the polygon with a particular color and title
 
 
-zoom(wrld_simpl, ext=drawExtent(), new=TRUE, useRaster=TRUE) #zooming on this raster
+#we can further explore the elements of the dataframe of the polygon as follows: 
+wrld_simpl@data #notice the '@' operator, this allows you to explore the objects inside spetial objects such as Spatial Polygons
 
+class(wrld_simpl@data) #by adding @data, we can see that it is a dataframe...
 
-crs(wrld_simpl)
+View (wrld_simpl@data) #the function View allows the user to see the polygon as an excel table, so super useful! 
 
-WGS84 = crs(wrld_simpl) # geographic projection
-e = drawExtent() #manually selection a portion of the map
+head (wrld_simpl@data) #watch the first six elements of the dataframe
 
-cp1 = as (extent(e), 'SpatialPolygons')
+#3.2.1 Subsetting polygons via its dataframe -----------------------
 
-mp1 = crop (wrld_simpl, cp1)
-plot (mp1)
+names (wrld_simpl@data) #check the names that define the columns of the dataframe
 
-
-cp2 = as(extent(-117.5538, -22.48155, -59.92064, 39.88928), 'SpatialPolygons') #selecting a portion of the map by coordinates
-proj4string(cp2) = CRS(proj4string(wrld_simpl))
-
-mp2 = crop (wrld_simpl, cp2)
-plot (mp2)
-
-wrld_simpl@data
-
-class (wrld_simpl@data)
-
-head (wrld_simpl@data)
-
-#subsetting data- Dataframes in action: 
-
-wrld_simpl@data$NAME
+wrld_simpl@data$NAME #use the operator $ to select the information in the column NAME
+#this show you all the countries defined in the dataframe of the shapefile 
 
 #function subset: 
-count1 = subset (wrld_simpl, NAME == 'Ecuador')
+count1 = subset (wrld_simpl, NAME == 'Ecuador') 
 plot (count1)
 
 count2 = subset (wrld_simpl, NAME == 'United States')
@@ -262,67 +325,77 @@ plot (count2)
 
 names (wrld_simpl@data)
 
-plot (wrld_simpl@data$AREA)
+plot (wrld_simpl@data$AREA) #this will create a plot with the data storaged in the AREA column
 
-exp1 = subset (wrld_simpl, AREA>500000)
+exp1 = subset (wrld_simpl, AREA>500000) #we can obtain only the countries with an area larger than 500000
 plot (exp1)
-plot (sort (wrld_simpl@data$POP2005))
 
-exp2 = subset (wrld_simpl, POP2005>1.1e+09)
-plot (exp2)
+plot (sort (wrld_simpl@data$POP2005)) #here we can use the column POP2005 in order to explore this data further
+
+exp2 = subset (wrld_simpl, POP2005>1.1e+09) #here we are selecting those countries with a population larger than 1.1e+09
+plot (exp2) #of course, India and China! 
 
 #write shapefile: 
-writeOGR(exp2, layer= 'exp2', dsn = 'shapes1', driver = 'ESRI Shapefile')
+writeOGR(exp2, layer= 'exp2', dsn = 'shapes2', driver = 'ESRI Shapefile') #we can write our second shape 
 
 
-#reading shapefiles: 
+#3.2.1 Downloading and manipulating reliable shape files ----------------------- 
 
+#' Open access and very detailed, the shape files from Natural Earth have 
+#' become very popular for any GIS application: 
+  
 download.file("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip", 
               destfile = 'countries.zip') #download the zip file 
 
 unzip(zipfile = "countries.zip", 
-      exdir = './countries_test') #unzipping the file 
+      exdir = './countries_test') #unzipping the file, in exdir you have to specify the folder name
 
 world2 = readOGR ('./countries_test/ne_10m_admin_0_countries.shp') #reading the file 
 
-class (world2)
-names (world2@data)
+class (world2) 
+names (world2@data) #check the name of the variables of this dataframe 
 
-View (world2@data)
+View (world2@data) 
 
 unique(world2$ADMIN) #information on the unique values of a column
 
 am = subset (world2, CONTINENT =='South America' 
-              | CONTINENT == 'North America')
-plot (am)
+              | CONTINENT == 'North America') #subsetting based on the variable continent 
+plot (am) #takes longer in plotting because it is far more detailed than the wrld_spl map
 
 zoom(am, ext=drawExtent(), new=TRUE, useRaster=TRUE) #zooming on this raster
 
 
-br1 = subset (world2, ADMIN == 'Brazil')
-plot (br1)
+br1 = subset (world2, ADMIN == 'Brazil') #we can select any particular country 
+plot (br1, col = 'grey', main = 'Brazil from Natural Earth')
 
 #write shapefile: 
-writeOGR(br1, layer= 'br1', dsn = 'shapes2', driver = 'ESRI Shapefile')
+writeOGR(br1, layer= 'br1', dsn = 'shapes3', driver = 'ESRI Shapefile') #write the new country
+
 
 #3. GIS objects II: Points -----------------------------------
 
 #points in a map: 
 
-pt_random = spsample (br1, n = 100, 'random')
+pt_random = spsample (br1, n = 100, 'random') #this function will throw random points in the polygon of your selection 
 
-class(pt_random)
+class(pt_random) #is a spatial points object 
 
 plot (br1)
-points (pt_random, pch= 3, cex = 0.7, col= 'red')
+points (pt_random, pch= 3, cex = 0.7, col= 'red') #plotting the points in the map 
 
 #writing points 
-write.csv (pt_random@coords, 'test1.csv', row.names = F)
+write.csv (pt_random@coords, 'test1.csv', row.names = F) #writing the points as a csv file 
 
 
 #NEXT EXCERCISES ---------------------------------------
 
-#4. GIS objects II: Rasters -----------------------------------
+#' csv files 
+#' reading csv files 
+#' decimal cooordinates 
+#' 
+
+#4. GIS objects III: Rasters -----------------------------------
 
 #what is a raster
 #creating rasters 
